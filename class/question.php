9e390ee58e1ac607ca590cb2d2a073513d0f5d1e
RomainGARNIER <romain.garnier@etu.unice.fr>
2016-10-10 10:52:04 +0200

    Modification de la classe question et de son premier test

--- class/question.php
+++ class/question.php
@@ -8,6 +8,7 @@ class Question {
   var $_indice;
   var $_nb_choix;
 
+
   function __construct($unePhrase, $uneSolution, $unIndice, $unNbChoix){
     $this->_phrase = $unePhrase;
     $this->_solution = $uneSolution;
@@ -15,17 +16,81 @@ class Question {
     $this->_nb_choix = $unNbChoix;
   }
 
-  function save(){
+  function save($id_theme){
+    global $bdd;
     try{
-      $bdd->query("INSERT INTO theme(q_phrase,q_solution,q_indice,q_nb_choix) VALUES ($this->_id = $unId,this->_phrase,$this->_solution,$this->_indice,$this->_nb_choix)");
+      $bdd->query("INSERT INTO question(q_phrase,q_solution,q_indice,q_nb_choix,q_t_id) VALUES ('$this->_phrase','$this->_solution','$this->_indice','$this->_nb_choix','$id_theme')");
     }catch(Exception $e)
     {
       die('Erreur : '.$e->getMessage());
     }
+    $this->_id = $bdd->lastInsertId();
+  }
+
+  function getId(){
+    return $this->_id;
+  }
+
+  function getPhrase(){
+    return $this->_phrase;
+  }
+
+  function getSolution(){
+    return $this->_solution;
+  }
+
+  function getIndice(){
+    return $this->_indice;
+  }
+
+  function getNbChoix(){
+    return $this->_nb_choix;
+  }
+
+  function getChoix(){
+    return $this->_choix;
+  }
+
+  function setId($id){
+    $this->_id=$id;
+  }
+
+
+  // Converti un objet SQL en Question
+  // Testé
+  static function getSQLObject($SQLObject){
+    $question = new Question($SQLObject["q_phrase"],$SQLObject["q_solution"],$SQLObject["q_indice"],$SQLObject["q_nb_choix"],$SQLObject["q_t_id"]);
+    $question->setId($SQLObject["q_id"]);
+    return $question;
+  }
+
+  // Méthode renvoyant une liste de tous les thèmes
+  // Testé
+  static function getAllQuestions(){
+    global $bdd;
+    if ($result = $bdd->query("SELECT * FROM `question`")){
+         // Cycle through results
+         $group_arr=null;
+        while ($row = $result->fetch()){
+            $group_arr[] = Question::getSQLObject($row);
+        }
+         // Free result set
+         $result->closeCursor();
+         return $group_arr;
+    }
+  }
+
+  static function getQuestion($id){
+    global $bdd;
+    if ($resultSQL = $bdd->query("SELECT * FROM `question` WHERE q_id=$id")){
+      $result = $resultSQL->fetch();
+      $resultSQL->closeCursor();
+      return $result;
+    }
   }
 
   function addChoix($unChoix){
-    
+
   }
 
 }
---
+++ tests/question_test_1.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
+
+  </head>
+
+<?php
+include "../class/question.php";
+include "../class/theme.php";
+include "../connexion_bdd.php";
+
+if(!empty($_POST)){
+  $question = new Question($_POST["phrase"],$_POST["solution"],$_POST["indice"],$_POST["nbChoix"]);
+  print_r($_POST["phrase"]." ".$_POST["solution"]." ".$_POST["indice"]." ".$_POST["nbChoix"]);
+  print_r($question->getId()." ".$question->getPhrase()." ".$question->getSolution()." ".$question->getIndice()." ".$question->getNbChoix());
+  $question->save($_POST["theme"]);
+}
+
+?>
+
+  <body>
+    <pre>
+
+      <?php
+      if(($themesList = Theme::getAllThemes())!=null){
+        echo '<select name="theme" form="formQuestion">';
+        foreach ($themesList as $key => $value) {
+          echo "<option value=".$value->getId().">".$value->getNom()."</option>";
+        }
+        echo "</select>";
+      }
+      ?>
+
+      <form id="formQuestion" class="" action="#" method="post">
+        <label for="">Question</label>        <input type="text" name="phrase" value=""><br>
+        <label for="">Solution</label>        <input type="text" name="solution" value=""><br>
+        <label for="">Indice</label>          <input type="text" name="indice" value=""><br>
+        <label for="">Nombre de choix</label> <input type="number" name="nbChoix" value=""><br>
+        <input type="submit" name="name" value="valider">
+      </form>
+
+
+
+      <label>Liste des questions</label>
+    </pre>
+
+    <?php
+      if(($questionsList = Question::getAllQuestions())!=null){
+        foreach ($questionsList as $key => $value) {
+          //print_r($value);
+          echo "<label id=".$value->getId().">Question : ".$value->getPhrase().", solution : ".$value->getSolution().", nombre de choix: ".$value->getNbChoix().", indice: ".$value->getIndice()."<label><br>";
+        }
+      }else echo "Il n'existe aucune questions";
+    ?>
+
+  </body>
+</html>
