94e9d5362cab8b7269c0eaed91636d01e1bd786c
Damien Rogé <damien.roge.info@gmail.com>
2016-10-12 16:33:41 +0200

    Maj prof
    Juste pour avoir le changement sur mon pc fixe, rien de fou...

--- prof/index.php
+++ prof/index.php
@@ -7,29 +7,18 @@
 </head>
 <body>
     <header>
-        <h1 id="foo">Votre examen</h1>
+        <h1>Votre examen</h1>
         <h2>Thèmes</h2>
     </header>
     <div class="theme-form-container">
         <form id="input-form">
-          <input type="text" id="nom_theme" placeholder="Entrez votre thème ici..."/>
+          <input type="text" id="nom_theme" autocomplete="off" placeholder="Entrez votre thème ici..."/>
           <input type="submit" value="Ajouter" />
         </form>
     </div>
 
     <ul class="themes-container">
 
-        <div class="question-form-container">
-        </div>
-        <ul class="question-container">
-
-            <div class="choix-form-container">
-            </div>
-
-            <ul class="choix-container">
-            </ul>
-
-        </ul>
     </ul>
 </body>
 
--- prof/js/script.js
+++ prof/js/script.js
@@ -1,13 +1,13 @@
 (function(){
 
     var init = function(){
-        handleInput();
+        handleInputThemes();
 
 
 
     }
 
-    function handleInput(){
+    function handleInputThemes(){
         $("#input-form").on('submit',function(event){
             var input_nom_theme = $("#nom_theme");
             event.preventDefault(); //supprimer l'event de base du submit
@@ -21,11 +21,46 @@
 
     }
 
-    function ajouterThemeToList(nom){
-        var ul_themes = $(".themes-container");
-        ul_themes.append("<li>"+nom+"</li>");
+    //Appelé à la création du input pour parametré l'ajout de l'entitulé de la question dans le  bon <ul>
+    function handleInputQuestions(id_input){
+      $("#input-form-question-"+id_input).on('submit',function(event){
+          var input_nom_theme = $("#"+id_input+"nom_question");
+          event.preventDefault(); //supprimer l'event de base du submit
+          nom_theme = input_nom_theme.val();
+          console.log(nom_theme);
+          if(nom_theme){
+              ajouterThemeToList(nom_theme);
+              input_nom_theme.val('');
+          }
+      });
+
     }
 
+    function ajouterThemeToList(nom){
+        var ul_themes = $(".themes-container");
+
+        var form = $("<form></form>");
+        form.attr("id","question-form-"+compterThemes());
+
+
+        ul_themes.prepend(form);
+        form.after("<li>"+nom+"</li>");
+        form.append("<input type=\"text\" placeholder=\"Votre question ici...\" autocomplete=\"off\"/>");
+        form.append("<input type=\"submit\" value=\"Ajouter\"/>");
+
+        form.after("<ul class=\"questions-container\" id=\"\"></ul>");
+
+
+    }
+
+    function ajouterInputQuestion(nom_theme){
+
+
+    }
+
+    function compterThemes(){
+      return $("#themes-container li").length;
+    }
 
 
 
